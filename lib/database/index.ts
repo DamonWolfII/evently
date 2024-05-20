51a5d8db3ec14f9bc2fde1b902b4b6e09a9c7881
DamonWolfII <utsav2071@outlook.com>
2024-05-20 18:40:01 +0545

    Mongoose

---
+++ lib/database/index.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let cashed = (global as any).mongoose || {conn : null, promise: null};
+
+export const connecttoDatabase = async () => {
+    if (cashed.conn) return cashed.conn;
+
+    if(!MONGODB_URI) throw new Error('MongoDB_URI missing');
+
+    cashed.promise = cashed.promise || mongoose.connect(MONGODB_URI, {
+        dbName: 'evemtly',
+        bufferCommands: false,
+    })
+
+    cashed.conn = await cashed.promise;
+
+    return cashed.conn;
+
+}
